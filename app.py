9ca28238ff3aaf789d52398bc96516a83f023c35
Zonayed Ahamad <zonayed320@gmail.com>
2024-07-11 19:13:45 +0600

    Make server for oparetions

--- app.py
+++ app.py
@@ -1,14 +1,78 @@
-from haystack.nodes import EmbeddingRetriever
+from haystack.nodes import EmbeddingRetriever, PreProcessor
 from haystack.document_stores import WeaviateDocumentStore
-from haystack import Pipeline
+from haystack import Document, Pipeline
+from fastapi import FastAPI, Request, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+import uvicorn
+import uuid
 
 # Document store setup
-document_store = WeaviateDocumentStore(host='http://localhost', port=8080, embedding_dim=384)  # Ensure embedding_dim matches the model
+document_store = WeaviateDocumentStore(
+    host='http://localhost', port=8080, embedding_dim=384)
 
 retriever = EmbeddingRetriever(
     document_store=document_store,
     embedding_model="sentence-transformers/all-MiniLM-L6-v2"
 )
+
+app = FastAPI()
+
+# Body payload type of ingest
+# {
+#  "content": "Welche Staaten sind gemäß der Bekanntmachung über die Feststellung der Gegenseitigkeit gemäß § 1 Abs. 2 des Auslandsunterhaltsgesetzes vom 20. Juli 1987 (BGBl. 1987 II S. 420) als Gegenseitigkeitsstaaten anerkannt?",
+#  "meta": {
+#    "source_url": "https://www.ris.bka.gv.at/GeltendeFassung.wxe?Abfrage=Bundesnormen&Gesetzesnummer=20000516&FassungVom=20220101",
+#    "pdf_name": "AUG§1Abs2Bek_07-1987.pdf"
+# }
+
+
+@app.post("/ingest")
+async def ingest(request: Request):
+    data = await request.json()
+    content = data.get("content")
+    meta = data.get("meta")
+
+    if not content or not meta:
+        return Response("Both 'content' and 'meta' are required.", status_code=400)
+
+    preprocessor = PreProcessor(
+        clean_empty_lines=True,
+        clean_whitespace=True,
+        clean_header_footer=True,
+        split_by="word",
+        split_length=500,
+        split_respect_sentence_boundary=True,
+        split_overlap=0
+    )
+
+    doc = Document(content=content, meta=meta, id=str(uuid.uuid4()))
+    preprocessed_docs = preprocessor.process([doc])
+
+    pdf_name = meta.get("pdf_name")
+    if pdf_name:
+        existing_docs = document_store.get_all_documents(
+            filters={"meta.pdf_name": pdf_name})
+        if existing_docs:
+            return Response("Document already exists in the document store.", status_code=400)
+    else:
+        # Handle the case where "pdf_name" is missing in the meta dictionary
+        return Response("'pdf_name' is missing in the 'meta' dictionary.", status_code=400)
+
+    embeddings = retriever.embed_documents(preprocessed_docs)
+
+    # print(embeddings)
+    for doc, emb in zip(preprocessed_docs, embeddings):
+        doc.embedding = emb
+
+    document_store.write_documents(preprocessed_docs)
+
+    # preprocessed_docs[0].embedding = embeddings[0]
+
+    # document_store.write_documents(preprocessed_docs)
+
+    return Response("Document ingested successfully.", status_code=200)
+
 
 def get_result(query):
     pipeline = Pipeline()
@@ -17,49 +81,19 @@ def get_result(query):
     response = pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
     return response
 
-# Question generated from "AUG§1Abs2Bek_07-1987.pdf"
-results = get_result("Welche Staaten sind gemäß der Bekanntmachung über die Feststellung der Gegenseitigkeit gemäß § 1 Abs. 2 des Auslandsunterhaltsgesetzes vom 20. Juli 1987 (BGBl. 1987 II S. 420) als Gegenseitigkeitsstaaten anerkannt?")
+
+@app.post("/result")
+async def result(request: Request):
+    data = await request.json()
+    query = data.get("query")
+
+    if not query:
+        return Response("'query' is required.", status_code=400)
+
+    results = get_result(query)
+    response_data = jsonable_encoder(results)
+    return JSONResponse(content=response_data)
 
 
-# Question generated from "9._RAV.pdf"
-# results = get_result("Welche Anpassungen der Rentenwerte und Unfallversicherungsleistungen in dem in Artikel 3 des Einigungsvertrages genannten Gebiet werden in der Verordnung zur neunten Anpassung der Renten (9. Rentenanpassungsverordnung - 9. RAV) vom 12. Dezember 1994 (BGBl. I S. 3805) festgelegt?")
-
-
-
-for result in results["documents"]:
-    print(f"\033[92mscore: {result.score}\033[0m")
-    print(f"content: {result.content[0:100]}")
-    print(f"metadata: {result.meta}")
-    print("============================")
-
-for key, value in results.items():
-    if key != "documents":
-        print(f"{key}: {value}")
-
-
-
-
-
-
-
-# Model Entigration with HayStack done here;
-# def get_result(query):
-#     pipeline = Pipeline()
-#     pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
-
-#     prompt_model = PromptModel(model_name_or_path="gpt-3.5-turbo")
-#     prompt_node = PromptNode(prompt_model)
-
-#     prompt_template = PromptTemplate(
-#         name="question-answering",
-#         prompt_text="""Given the provided Documents, answer the Query. Make your answer detailed and long
-#                        Query: {query}
-#                        Documents: {join(documents)}
-#                        Answer: """
-#     )
-
-#     pipeline.add_node(component=prompt_node, name="PromptNode", inputs=["Retriever"])
-#     pipeline.add_node(component=prompt_template, name="PromptTemplate", inputs=["PromptNode"])
-
-#     response = pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
-#     return response
+if __name__ == "__main__":
+    uvicorn.run("app:app", host='0.0.0.0', port=3001)
